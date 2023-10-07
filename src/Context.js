e18fe0f3f5e4a129fb5419e618c8fac6de2e4e47
alanjames00 <alanjames6282@gmail.com>
2023-10-07 10:21:26 +0530

    add Context component to be used by Zustand and set initial context value false

---
+++ src/Context.js
@@ -0,0 +1,5 @@
+// Import necessary dependencies
+import { createContext } from 'react';
+
+// Create and export a React context with an initial value of `false`
+export const Context = createContext(false);
