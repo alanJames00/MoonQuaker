c46335beb949d9d6e2ddde9e884f393b326c86c0
alanjames00 <alanjames6282@gmail.com>
2023-10-07 10:14:17 +0530

    remove debug info, clean code, add comments in QuakeWave component

--- src/QuakeWave.js
+++ src/QuakeWave.js
@@ -1,3 +1,4 @@
+// Import necessary dependencies and components
 import React, { useRef, useEffect } from 'react';
 import { useGLTF, useAnimations } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
@@ -5,39 +6,48 @@ import { useFrame } from '@react-three/fiber';
 // Import Quake data collection
 import quakeData from './data/QuakeDB.json'
 
-// Import Context
+// Import custom store
 import useStore from "./Store";
 
 export default function Model({ camRef }) {
 
+  // Define empty object for props
   const props = {}
-
+  
+  // Access context from custom store
   const selectedYear = useStore((state) => state.year);
   const isPlaying = useStore((state) => state.isPlaying);
   const selectedDay = useStore((state) => state.day);
 
 
+  // Find earthquake data based on selectedYear and selectedDa
   const quake = quakeData.find((item) => item.year == selectedYear && item.day == selectedDay);
 
 
+  // Create ref for the group
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF('/assets/wave.glb');
-  const { actions } = useAnimations(animations, group);
+  const { nodes, materials, animations } = useGLTF('/assets/wave.glb');  // Destructure nodes, materials, and animations from the glTF model
+  const { actions } = useAnimations(animations, group); // Destructure actions from useAnimations
 
+  // Run the KeyAction animation when the component mounts
   useEffect(() => {
     actions.KeyAction.play();
   });
 
+
+  // Use useFrame to update the scene on each frame
   useFrame(() => {
-    group.current.lookAt(0, 0, 0);
+    group.current.lookAt(0, 0, 0); // Set group to look at origin (0, 0, 0)
     camRef.current.position.set(quake.longitude*0.02,quake.latitude*0.023,1.7)
   }, []);
 
 
+  // Calculate the radius based on earthquake magnitude and convert radians
   let r = quake.magnitude < 1 ? 1.99 : 1.97;
-  r = r * 0.50
+  r = r * 0.50;
   const degToRad = (deg) => (deg * Math.PI) / 180.0;
 
+  // Render the Wave Component
   return (
     <group
       {...props}
@@ -47,11 +57,15 @@ export default function Model({ camRef }) {
         name='Scene'
         ref={group}
         position={[
+          /*  Calculate the x, y, z - coordinates of the position based on latitude and longitude considering
+              Spherical coordinates
+          */
           r * Math.sin(Math.PI / 2 - degToRad(quake.latitude)) * Math.sin(degToRad(quake.longitude)),
           r * Math.cos(Math.PI / 2 - degToRad(quake.latitude)),
           r * Math.sin(Math.PI / 2 - degToRad(quake.latitude)) * Math.cos(degToRad(quake.longitude)),
         ]}
       >
+        {/* Create the mesh to render as Wave of moonquake */}
         <mesh
           name='Icosphere'
           geometry={nodes.Icosphere.geometry}
@@ -64,7 +78,8 @@ export default function Model({ camRef }) {
       </group>
     </group>
   );
-  
 }
 
+
+// Preload the wave glTF model
 useGLTF.preload('/assets/wave.glb');
